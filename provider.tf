91a96028eaf20be0a5de8dfc885e446fb15de863
OJT Jeb Vincent C. Cajayon <ojt.jcajayon@smic.sm.ph>
2025-03-17 17:19:00 +0800

    updated the provider.tf

--- provider.tf
+++ provider.tf
@@ -1,22 +1,14 @@
-# provider.tf
 terraform {
-  required_version = ">= 1.6"
-
-
+  required_version = ">= 1.4"
 
   required_providers {
-   aws = {
-     source  = "hashicorp/aws"
-     version = ">= 5.0"
-   }
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
   }
 }
 
-
-
 provider "aws" {
-  region     = "ap-southeast-1"
-  access_key = "insert your access key here"
-  secret_key = "insert your secret key here"
-  token      = "insert your token here"
+  region = "ap-southeast-1"
 }
