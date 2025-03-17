3bdf6857be936f35240931bbbcbf469aabc331ba
OJT Jeb Vincent C. Cajayon <ojt.jcajayon@smic.sm.ph>
2025-03-17 17:50:21 +0800

    updated the main.tf

--- main.tf
+++ main.tf
@@ -105,7 +105,8 @@ resource "aws_instance" "web" {
   tags = {
     Name            = "itss-ojt-Cajayon-ec2-v2"
     Environment     = "Sandbox"
-    Schedule        = "Running (disable)"
+    backup          = "no"
+    Schedule        = "running (disable)"
     Patch           = "No"
     Resource_Types  = "Instances Volumes Network_Interfaces"
   }
