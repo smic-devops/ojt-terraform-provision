6155b9f47a8fae00003e2d52f618b3c6df3ee221
OJT Jeb Vincent C. Cajayon <ojt.jcajayon@smic.sm.ph>
2025-03-17 17:05:36 +0800

    updated the backend.tf as it was showing error in github

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
-        bucket         = "itss-devops-ojt-tfstate"
-        key            = "tfstate-devops-ojt-david"
-        region         = "ap-southeast-1"
-        }
+        bucket = "itss-devops-ojt-tfstate"
+        key    = "tfstate-devops-ojt-cajayon"
+        region = "ap-southeast-1"
     }
+}
