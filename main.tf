a32aecddc07ee363e06403086b2f9880e1fcaa03
OJT Jeb Vincent C. Cajayon <ojt.jcajayon@smic.sm.ph>
2025-03-19 13:22:40 +0800

    updated the  main.tf

--- main.tf
+++ main.tf
@@ -7,21 +7,21 @@ resource "aws_security_group" "alb_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["sg-0f24a18c1aefde757"]
   }
   
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["sg-0f24a18c1aefde757"]
   }
   
   ingress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["sg-0f24a18c1aefde757"]
   }
 
   egress {
