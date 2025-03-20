047584a6020c881dbe8de4567ebaca788de07f8f
OJT Jeb Vincent C. Cajayon <ojt.jcajayon@smic.sm.ph>
2025-03-20 09:39:44 +0800

    updated the  main.tf

--- main.tf
+++ main.tf
@@ -75,6 +75,16 @@ resource "aws_lb_target_group" "tg" {
   protocol = "HTTP"
   vpc_id   = var.vpc_id
 
+  health_check {
+    interval            = 30
+    path                = "/"
+    port                = "80"
+    protocol            = "HTTP"
+    timeout             = 5
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+  }
+
   tags = {
     Environment    = "Sandbox"
     Resource_Types = "Instances Volumes Network_Interfaces"
